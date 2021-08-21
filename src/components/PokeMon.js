8aae6292271a1e02a85f7d9bf14ef7f761a23f8b
omidiora <omidioraemmanuel@gmail.com>
2021-08-21 23:13:34 +0100

    initial commit

---
+++ src/components/PokeMon.js
@@ -0,0 +1,59 @@
+import React, { useEffect, useState } from 'react'
+import PokemonThumb from '../components/PokemonThumb'
+
+
+
+const Pokemon= () => {
+
+    const[allPokemons, setAllPokemons] = useState([])
+    const [loadMore, setLoadMore] = useState('https://pokeapi.co/api/v2/pokemon?limit=20')
+    const [search, setSearch] = useState("");
+ 
+   const getAllPokemons = async () => {
+     const res = await fetch(loadMore)
+     const data = await res.json()
+ 
+     setLoadMore(data.next)
+ 
+     function createPokemonObject(results)  {
+       results.forEach( async pokemon => {
+         const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon.name}`)
+         const data =  await res.json()
+         setAllPokemons( currentList => [...currentList, data])
+         await allPokemons.sort((a, b) => a.id - b.id)
+       })
+     }
+     createPokemonObject(data.results)
+   }
+ 
+ 
+   
+  useEffect(() => {
+   getAllPokemons()
+  }, [])
+ 
+   return (
+     <div className="app-contaner">
+       <h1>Pokemon Evolution with Details</h1>
+ 
+ 
+       <div className="pokemon-container">
+         <div className="all-container">
+           {allPokemons.map( (pokemonStats, index) => 
+             <PokemonThumb
+               key={index}
+               id={pokemonStats.id}
+               image={pokemonStats.sprites.other.dream_world.front_default}
+               name={pokemonStats.name}
+               type={pokemonStats.types[0].type.name}
+             />)}
+           
+         </div>
+           <button className="load-more" onClick={() => getAllPokemons()}>Load more</button>
+       </div>
+     </div>
+   );
+ }
+ 
+ 
+export default Pokemon
---
+++ src/components/PokemonDetails.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+function PokemonDetails() {
+  return (
+    <>
+     
+    </>
+  );
+}
+
+export default PokemonDetails;
